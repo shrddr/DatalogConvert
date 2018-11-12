29fb103f95446f123509917b79ddb4b9ffe3e526
shrddr <qwerty-zigzag@mail.ru>
2018-11-12 16:00:00 +0300

    Add project files.

---
+++ DatalogConvert/Program.cs
@@ -0,0 +1,439 @@
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatalogConvert
+{
+    class Program
+    {
+        class TagTableRow
+        {
+            public string tn;
+            public Int16 ti;
+            public byte tt;
+            public Int16 td;
+            public int met;
+            public int used;
+            public Int16 newIndex;
+        }
+
+        class TagInfo
+        {
+            public string name;
+            public Int16 newIndex;
+            public byte tt;
+            public Int16 td;
+            public int met;
+            public int used;
+        }
+
+        class FloatTableRow
+        {
+            public DateTime dt;
+            public Int16 mi;
+            public Int16 ti;
+            public double v;
+            public char s;
+            public char m;
+            public Int16 newIndex;
+        }
+
+        static byte[] makeTagHeader(Int32 rowcount)
+        {
+            MemoryStream memStream = new MemoryStream();
+            BinaryWriter writer = new BinaryWriter(memStream);
+            int yy = DateTime.Now.Year - 1900;
+            int mm = DateTime.Now.Month;
+            int dd = DateTime.Now.Day;
+            writer.Write((byte)0x03);
+            writer.Write((byte)yy);
+            writer.Write((byte)mm);
+            writer.Write((byte)dd);
+            writer.Write(rowcount);
+            writer.Write(0x010800A1);
+            writer.Write(0x00000000);
+            writer.Write(0x00000000);
+            writer.Write(0x00000000);
+            writer.Write(0x00000000);
+            writer.Write(0x00000000);
+            writer.Write(Encoding.ASCII.GetBytes("Tagname".PadRight(11, '\0')));
+            writer.Write('C');
+            writer.Write(0x00000001);
+            writer.Write(0x000000FF);
+            writer.Write(0x00000000);
+            writer.Write(0x00000000);
+            writer.Write(0x00000000);
+            writer.Write(Encoding.ASCII.GetBytes("TTagIndex".PadRight(11, '\0')));
+            writer.Write('N');
+            writer.Write(0x00000100);
+            writer.Write(0x00000005);
+            writer.Write(0x00000000);
+            writer.Write(0x00000000);
+            writer.Write(0x00000000);
+            writer.Write(Encoding.ASCII.GetBytes("TagType".PadRight(11, '\0')));
+            writer.Write('N');
+            writer.Write(0x00000105);
+            writer.Write(0x00000001);
+            writer.Write(0x00000000);
+            writer.Write(0x00000000);
+            writer.Write(0x00000000);
+            writer.Write(Encoding.ASCII.GetBytes("TagDataTyp".PadRight(11, '\0')));
+            writer.Write('N');
+            writer.Write(0x00000106);
+            writer.Write(0x00000002);
+            writer.Write(0x00000000);
+            writer.Write(0x00000000);
+            writer.Write(0x00000000);
+            writer.Write((byte)0x0D);
+            return memStream.ToArray();
+        }
+
+        static byte[] makeFloatHeader(Int32 rowcount)
+        {
+            MemoryStream memStream = new MemoryStream();
+            BinaryWriter writer = new BinaryWriter(memStream);
+            int yy = DateTime.Now.Year - 1900;
+            int mm = DateTime.Now.Month;
+            int dd = DateTime.Now.Day;
+            writer.Write((byte)0x03);
+            writer.Write((byte)yy);
+            writer.Write((byte)mm);
+            writer.Write((byte)dd);
+            writer.Write(rowcount);
+            writer.Write(0x00270121);
+            writer.Write(0x00000000);
+            writer.Write(0x00000000);
+            writer.Write(0x00000000);
+            writer.Write(0x00000000);
+            writer.Write(0x00000000);
+            writer.Write(Encoding.ASCII.GetBytes("Date".PadRight(11, '\0')));
+            writer.Write('D');
+            writer.Write(0x00000001);
+            writer.Write(0x00000008);
+            writer.Write(0x00000000);
+            writer.Write(0x00000000);
+            writer.Write(0x00000000);
+            writer.Write(Encoding.ASCII.GetBytes("Time".PadRight(11, '\0')));
+            writer.Write('C');
+            writer.Write(0x00000009);
+            writer.Write(0x00000008);
+            writer.Write(0x00000000);
+            writer.Write(0x00000000);
+            writer.Write(0x00000000);
+            writer.Write(Encoding.ASCII.GetBytes("Millitm".PadRight(11, '\0')));
+            writer.Write('N');
+            writer.Write(0x00000011);
+            writer.Write(0x00000003);
+            writer.Write(0x00000000);
+            writer.Write(0x00000000);
+            writer.Write(0x00000000);
+            writer.Write(Encoding.ASCII.GetBytes("TagIndex".PadRight(11, '\0')));
+            writer.Write('N');
+            writer.Write(0x00000014);
+            writer.Write(0x00000005);
+            writer.Write(0x00000000);
+            writer.Write(0x00000000);
+            writer.Write(0x00000000);
+            writer.Write(Encoding.ASCII.GetBytes("Value".PadRight(11, '\0')));
+            writer.Write('C');
+            writer.Write(0x00000019);
+            writer.Write(0x00000008);
+            writer.Write(0x00000000);
+            writer.Write(0x00000000);
+            writer.Write(0x00000000);
+            writer.Write(Encoding.ASCII.GetBytes("Status".PadRight(11, '\0')));
+            writer.Write('C');
+            writer.Write(0x00000021);
+            writer.Write(0x00000001);
+            writer.Write(0x00000000);
+            writer.Write(0x00000000);
+            writer.Write(0x00000000);
+            writer.Write(Encoding.ASCII.GetBytes("Marker".PadRight(11, '\0')));
+            writer.Write('C');
+            writer.Write(0x00000022);
+            writer.Write(0x00000001);
+            writer.Write(0x00000000);
+            writer.Write(0x00000000);
+            writer.Write(0x00000000);
+            writer.Write(Encoding.ASCII.GetBytes("Internal".PadRight(11, '\0')));
+            writer.Write('C');
+            writer.Write(0x00000023);
+            writer.Write(0x00000004);
+            writer.Write(0x00000000);
+            writer.Write(0x00000000);
+            writer.Write(0x00000000);
+            writer.Write((byte)0x0D);
+            return memStream.ToArray();
+        }
+
+        static byte[] makeStringHeader(Int32 rowcount)
+        {
+            MemoryStream memStream = new MemoryStream();
+            BinaryWriter writer = new BinaryWriter(memStream);
+            int yy = DateTime.Now.Year - 1900;
+            int mm = DateTime.Now.Month;
+            int dd = DateTime.Now.Day;
+            writer.Write((byte)0x03);
+            writer.Write((byte)yy);
+            writer.Write((byte)mm);
+            writer.Write((byte)dd);
+            writer.Write(rowcount);
+            writer.Write(0x00710121);
+            writer.Write(0x00000000);
+            writer.Write(0x00000000);
+            writer.Write(0x00000000);
+            writer.Write(0x00000000);
+            writer.Write(0x00000000);
+            writer.Write(Encoding.ASCII.GetBytes("Date".PadRight(11, '\0')));
+            writer.Write('D');
+            writer.Write(0x00000001);
+            writer.Write(0x00000008);
+            writer.Write(0x00000000);
+            writer.Write(0x00000000);
+            writer.Write(0x00000000);
+            writer.Write(Encoding.ASCII.GetBytes("Time".PadRight(11, '\0')));
+            writer.Write('C');
+            writer.Write(0x00000009);
+            writer.Write(0x00000008);
+            writer.Write(0x00000000);
+            writer.Write(0x00000000);
+            writer.Write(0x00000000);
+            writer.Write(Encoding.ASCII.GetBytes("Millitm".PadRight(11, '\0')));
+            writer.Write('N');
+            writer.Write(0x00000011);
+            writer.Write(0x00000003);
+            writer.Write(0x00000000);
+            writer.Write(0x00000000);
+            writer.Write(0x00000000);
+            writer.Write(Encoding.ASCII.GetBytes("TagIndex".PadRight(11, '\0')));
+            writer.Write('N');
+            writer.Write(0x00000014);
+            writer.Write(0x00000005);
+            writer.Write(0x00000000);
+            writer.Write(0x00000000);
+            writer.Write(0x00000000);
+            writer.Write(Encoding.ASCII.GetBytes("Value".PadRight(11, '\0')));
+            writer.Write('C');
+            writer.Write(0x00000019);
+            writer.Write(0x00000052);
+            writer.Write(0x00000000);
+            writer.Write(0x00000000);
+            writer.Write(0x00000000);
+            writer.Write(Encoding.ASCII.GetBytes("Status".PadRight(11, '\0')));
+            writer.Write('C');
+            writer.Write(0x0000006B);
+            writer.Write(0x00000001);
+            writer.Write(0x00000000);
+            writer.Write(0x00000000);
+            writer.Write(0x00000000);
+            writer.Write(Encoding.ASCII.GetBytes("Marker".PadRight(11, '\0')));
+            writer.Write('C');
+            writer.Write(0x0000006C);
+            writer.Write(0x00000001);
+            writer.Write(0x00000000);
+            writer.Write(0x00000000);
+            writer.Write(0x00000000);
+            writer.Write(Encoding.ASCII.GetBytes("Internal".PadRight(11, '\0')));
+            writer.Write('C');
+            writer.Write(0x0000006D);
+            writer.Write(0x00000004);
+            writer.Write(0x00000000);
+            writer.Write(0x00000000);
+            writer.Write(0x00000000);
+            writer.Write((byte)0x0D);
+            return memStream.ToArray();
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length < 4)
+            {
+                Console.WriteLine("Error: argument not found");
+                Console.WriteLine("Usage: DatalogConvert ServerName CatalogName User Password");
+                Console.ReadLine();
+                return;
+            }
+                
+            string DBServer = args[0];
+            string DBCatalog = args[1];
+            string DBUser = args[2];
+            string DBPassword = args[3];
+            List<TagTableRow> TagTableRows = new List<TagTableRow>();
+            List<DateTime> Dates = new List<DateTime>();
+
+            string connString = string.Format(
+                "Data Source={0};Initial Catalog={1};Persist Security Info=True;User ID={2};Password={3}",
+                DBServer,
+                DBCatalog,
+                DBUser,
+                DBPassword
+            );
+
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                try
+                {
+                    conn.Open();
+                    string sql = "SELECT * FROM TagTable ORDER BY TagIndex";
+                    SqlCommand cmd = new SqlCommand(sql, conn);
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        string tn = reader.GetString(0);
+                        Int16 ti = reader.GetInt16(1);
+                        byte tt = (byte)reader.GetInt16(2);
+                        Int16 td = reader.GetInt16(3);
+                        TagTableRows.Add(new TagTableRow() { tn = tn, ti = ti, tt = tt, td = td });
+                    }
+                    reader.Close();
+                    Console.WriteLine("Tags: " + TagTableRows.Count);    
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error reading tags: " + ex.Message);
+                }
+
+                try
+                {
+                    string sql = "SELECT DISTINCT(CAST(DateAndTime AS DATE)) FROM FloatTable";
+                    SqlCommand cmd = new SqlCommand(sql, conn);
+                    cmd.CommandTimeout = 600;
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        DateTime dt = reader.GetDateTime(0);
+                        Dates.Add(dt);
+                    }
+                    reader.Close();
+                    Console.WriteLine("Dates: " + Dates.Count);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error reading dates: " + ex.Message);
+                }
+
+                try
+                {
+                    foreach (DateTime day in Dates)
+                    {
+                        for (int i = 0; i < TagTableRows.Count; i++)
+                        {
+                            TagTableRows[i].met = 0;
+                            TagTableRows[i].used = 0;
+                            TagTableRows[i].newIndex = 0;
+                        }
+
+                        List<FloatTableRow> FloatTableRows = new List<FloatTableRow>();
+                        var TagData = new Dictionary<Int16, TagInfo>();
+
+                        string sql = "SELECT * FROM FloatTable WHERE DATEDIFF(day, DateAndTime, @DT) = 0 ORDER BY DateAndTime, TagIndex";
+                        SqlCommand cmd = new SqlCommand(sql, conn);
+                        cmd.CommandTimeout = 600;
+                        cmd.Parameters.AddWithValue("DT", day);
+                        SqlDataReader reader = cmd.ExecuteReader();
+
+                        Int16 newTagIndex = 0;
+                        while (reader.Read())
+                        {
+                            DateTime dt = reader.GetDateTime(0);
+                            Int16 mi = reader.GetInt16(1);
+                            Int16 ti = reader.GetInt16(2);
+                            double v = reader.GetDouble(3);
+                            string str = reader.GetString(4);
+                            char s = str[0];
+                            str = reader.GetString(5);
+                            char m = str[0];
+
+                            if (!TagData.ContainsKey(ti))
+                            {
+                                TagTableRow t = TagTableRows.Find(x => x.ti == ti);
+                                TagData.Add(t.ti, new TagInfo() { name = t.tn, newIndex = newTagIndex++, tt = t.tt, td = t.td, met = 1, used = 0 });
+                                FloatTableRows.Add(new FloatTableRow() { dt = dt, mi = mi, ti = ti, v = v, s = 'S', m = 'B', newIndex = TagData[ti].newIndex });
+                            }
+                            else
+                            {
+                                FloatTableRows.Add(new FloatTableRow() { dt = dt, mi = mi, ti = ti, v = v, s = s, m = m, newIndex = TagData[ti].newIndex });
+                                TagData[ti].met++;
+                            }
+                        }
+                        reader.Close();
+
+                        string dayString = day.ToString("yyyy MM dd");
+                        Console.WriteLine(dayString + " values: " + FloatTableRows.Count);
+                        string dayTagFile = dayString + " 0000 (Tagname).DAT";
+                        string dayFloatFile = dayString + " 0000 (Float).DAT";
+                        string dayStringFile = dayString + " 0000 (String).DAT";
+                        byte[] tagHeader = makeTagHeader(TagData.Count);
+                        byte[] floatHeader = makeFloatHeader(FloatTableRows.Count);
+                        byte[] stringHeader = makeStringHeader(0);
+                        Int32 internalCounter = 0;
+                        
+                        using (BinaryWriter writer = new BinaryWriter(File.Open(dayFloatFile, FileMode.Create)))
+                        {
+                            writer.Write(floatHeader);
+                            foreach (FloatTableRow f in FloatTableRows)
+                            {
+                                writer.Write((byte)0x20);
+                                writer.Write(Encoding.ASCII.GetBytes(f.dt.ToString("yyyyMMdd")));
+                                writer.Write(Encoding.ASCII.GetBytes(f.dt.ToString("HH:mm:ss")));
+                                writer.Write(Encoding.ASCII.GetBytes(f.dt.ToString("fff")));
+                                writer.Write(Encoding.ASCII.GetBytes(f.newIndex.ToString().PadLeft(5, ' ')));
+                                writer.Write(f.v);
+                                writer.Write(f.s);
+
+                                TagData[f.ti].used++;
+
+                                if (TagData[f.ti].used == TagData[f.ti].met)
+                                {
+                                    writer.Write('E');
+                                    writer.Write(internalCounter++);
+                                }
+                                else
+                                {
+                                    writer.Write(f.m);
+
+                                    if (f.m == 'B')
+                                        writer.Write(0xFFFFFFFF);
+                                    else
+                                        writer.Write(internalCounter++);
+                                }
+                            }
+                        }
+                        using (BinaryWriter writer = new BinaryWriter(File.Open(dayTagFile, FileMode.Create)))
+                        {
+                            writer.Write(tagHeader);
+                            foreach (TagInfo t in TagData.Values)
+                            {
+                                if (t.met == 0) continue;
+                                writer.Write((byte)0x20);
+                                writer.Write(Encoding.ASCII.GetBytes(t.name.ToUpper().PadRight(255, ' ')));
+                                writer.Write(Encoding.ASCII.GetBytes(t.newIndex.ToString().PadLeft(5, ' ')));
+                                writer.Write(Encoding.ASCII.GetBytes(t.tt.ToString().PadLeft(1, ' ')));
+                                writer.Write(Encoding.ASCII.GetBytes(t.td.ToString().PadLeft(2, ' ')));
+                            }
+                            writer.Write((byte)0x1A);
+                        }
+                        using (BinaryWriter writer = new BinaryWriter(File.Open(dayStringFile, FileMode.Create)))
+                            writer.Write(stringHeader);
+
+                        //break; // one day only
+                    }
+                    
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error reading values: " + ex.Message);
+                }
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DatalogConvert/readme.md
@@ -0,0 +1,25 @@
+This tool fetches data from FactoryTalk SQL-based datalog and creates File Set datalog files with the same content. This might help with your trend performance.
+
+How to use: first make sure you can connect to the SQL Server. Then run:
+
+```js
+DatalogConvert "PCname\SQLinstnce" "master" "user" "password"
+```
+
+This should generate a bunch of daily DAT files like this:
+
+```
+2018 10 27 0000 (Float).DAT
+2018 10 27 0000 (String).DAT
+2018 10 27 0000 (Tagname).DAT
+2018 10 28 0000 (Float).DAT
+2018 10 28 0000 (String).DAT
+2018 10 28 0000 (Tagname).DAT
+2018 10 30 0000 (Float).DAT
+2018 10 30 0000 (String).DAT
+2018 10 30 0000 (Tagname).DAT
+```
+
+Place the DAT files into your HMI Projects\AppName\DLGLOG\DatalogName. If the trend doesn't see the data generated this way, look into the DLG file. My guess is you can simply delete it.
+
+Make sure you switch the datalog storage format from ODBC to File Set so it starts reading the DAT files. Even that is not enough; in my tests, the trend was still reading **both** the SQL and file sets. If you don't want that, change the ODBC settings so the SQL server is no longer accessible (enter a non-existing username or remove the dsn file).
